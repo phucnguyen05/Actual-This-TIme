6255a2c863e0406aeebf31efa415f6cc80467d19
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-01 13:38:45 +0930

    question2

---
+++ main-1-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+extern int is_identity(int array[10][10]);
+
+int main(){
+    int identity_matrix[10][10]={{1,0,0,0,0,0,0,0,0,0},{0,1,0,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0,0},{0,0,0,1,0,0,0,0,0,0},{0,0,0,0,1,0,0,0,0,0},{0,0,0,0,0,1,0,0,0,0},{0,0,0,0,0,0,1,0,0,0},{0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,0,1,0},{0,0,0,0,0,0,0,0,0,1}};
+    int result = is_identity(identity_matrix);
+    std::cout<<result<<std::endl;
+    return 0;
+}
