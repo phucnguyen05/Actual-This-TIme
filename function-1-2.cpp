727bb6157f1d27a2cc6610dbc7b536f7417e11d3
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-01 13:40:49 +0930

    fix

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,10 +4,12 @@ int is_identity(int array[10][10]){
             if (i==j){
                 if(array[i][j]!=1){
                     return 0;
+                    break;
                 }
             }else{
                 if(array[i][j]!=0){
                     return 0;
+                    break;
                 }
 
             }
