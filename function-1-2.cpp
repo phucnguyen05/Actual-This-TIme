dcb80d597315eb9cca3396cece718fbfecffa3b2
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-01 13:34:31 +0930

    question 2

---
+++ function-1-2.cpp
@@ -0,0 +1,17 @@
+int is_identity(int array[10][10]){
+    for(int i=0;i<10;i++){
+         for(int j=0;i<10;i++){
+            if (i==j){
+                if(array[i][j]!=1){
+                    return 0;
+                }
+            }else{
+                if(array[i][j]!=0){
+                    return 0;
+                }
+
+            }
+        }
+    }
+    return 1; 
+}
