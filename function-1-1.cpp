d018cfb28c3d5d8c98d51e18ff39a1e542c8cefa
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-08 13:23:17 +0930

    first fix

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,8 +1,8 @@
-int arrayMin(double* array_pointer,int size){
-    int minimum_so_far=array_pointer[0];
+double arrayMin(double* array,int size){
+    double minimum_so_far=array[0];
     for (int i=1;i<size;i++){
-        if (array_pointer[i]<minimum_so_far){
-            minimum_so_far=array_pointer[i];
+        if (array[i]<minimum_so_far){
+            minimum_so_far=array[i];
         }
     }
     return minimum_so_far;
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -2,8 +2,8 @@ extern int arrayMin(double* array_pointer,int size);
 #include <iostream>
 
 int main(){
-    double double_array[]={125,326,12,854,125};
-    int minimum=arrayMin(double_array,5);
+    double double_array[]={1.2,4.9,13.7};
+    double minimum=arrayMin(double_array,3);
     std::cout<<minimum<<std::endl;
     return 1;
 }
