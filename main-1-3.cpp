522dbb0b4f649f7d7d76b0c99707873463472a5c
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-15 14:40:07 +0930

    fix

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,11 +1,9 @@
 #include "Person.h"
 #include <string>
 extern PersonList deepCopyPersonList(PersonList pl);
-extern PersonList createPersonList(int n);
 
 int main(){
     PersonList test1,test2;
-    test1=createPersonList(3);
     test2=deepCopyPersonList(test1);
     return 1;
 }
