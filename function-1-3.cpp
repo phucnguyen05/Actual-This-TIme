a27f0ac672f7079a3a705c6d03998eb854a36f98
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-15 14:36:52 +0930

    fix

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -4,11 +4,10 @@
 PersonList deepCopyPersonList(PersonList pl){
     PersonList deep_copy;
     deep_copy.numPeople=pl.numPeople;
-    deep_copy.people=new Person[pl.numPeople]
+    deep_copy.people=new Person[pl.numPeople];
     for (int i=0;i<pl.numPeople;i++){
         deep_copy.people[i].name=pl.people[i].name;
         deep_copy.people[i].age=pl.people[i].age;
-
     }
     return deep_copy;
 }
