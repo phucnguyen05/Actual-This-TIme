39d898f5b3cf2a0e1566560893066f321e9bca5c
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-08-30 19:33:04 +0930

    test

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,8 +4,26 @@
 #include "Car.h"
 #include "ParkingLot.h"
 #include <iostream>
+using namespace std;
 
 int main(){
     ParkingLot test(10);
+    for (int i=0;i<10;i++){
+        cout<<"enter Vehicle: "<<endl;
+        char user_input[100];
+        cin>> user_input;
+        if(user_input=="Car"){
+            test.parkVehicle(new Car(i));
+        }else if(user_input=="Bus"){
+            test.parkVehicle(new Bus(i));
+        }else if(user_input=="Motorbike"){
+            test.parkVehicle(new Motorbike(i));
+        }
+    }
+    int remove_id;
+    cout << "which vehicle to remove";
+    cin >> remove_id;
+    test.unparkVehicle(remove_id);
+
     return 1;
 }
