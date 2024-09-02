3191ed21cbc3e7a1379b89e3ffce4d87aaa29216
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-09-02 17:15:29 +0930

    test

--- Fridge.cpp
+++ Fridge.cpp
@@ -4,4 +4,21 @@ double Fridge::getPowerConsumption(){
     return powerRating*24*(volume/100);
 }
 
+Fridge::Fridge(){
+
+}
+
+double Fridge::getVolume(){
+    return volume;
+}
+
+void Fridge::setVolume(double volume){
+    this->volume=volume;
+}
+
+Fridge::Fridge(int powerRating,double volume){
+    this->powerRating=powerRating;
+    this->volume=volume;
+}
+
 
