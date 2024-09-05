1b1742bf443a7f85673185521e1eb8f03f858ecc
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-09-05 14:56:34 +0930

    test

--- Ford.cpp
+++ Ford.cpp
@@ -1,8 +1,19 @@
 #include "Ford.h"
 
+Ford::Ford(){}
+Ford::Ford(int badgeNumber, int price){
+    this->badgeNumber=badgeNumber;
+    this->price=price;
+}
+
+
 void Ford::refuel(int litres){
     if(litresOfFuel+litres<=60){
         litresOfFuel=litresOfFuel+litres;
     }
-
 }
+int Ford::get_litresOfFuel(){
+    return litresOfFuel;
+}
+
+
