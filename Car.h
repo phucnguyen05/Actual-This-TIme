50bcb03f67b805b1cee43ec9cd0120f37e87bdbb
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-09-05 13:55:24 +0930

    test

--- Car.h
+++ Car.h
@@ -4,11 +4,11 @@ class Car{
 public: 
     Car();
     Car(int price);   // creates a Car with a price
-    virtual void drive(int kms);
     int get_price();
     void set_price(int price);
     int get_emissions();
     void set_emissions(int emissions);
+    virtual void drive(int kms);
 protected:
     int price;
     int emissions;   // CO2 Emissions
