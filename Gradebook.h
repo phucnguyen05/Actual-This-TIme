630b6d8cf4788452fd87c44539156efbfb30465c
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-09-10 08:50:50 +0930

    test

--- Gradebook.h
+++ Gradebook.h
@@ -2,7 +2,7 @@
 #define GRADEBOOK_H
 #include <string>
 #include "Grade.h"
-class Gradebook{
+class Gradebook:public Grade{
 protected: 
     void addGrade(int stud_id,int course_id,std::string assignment, int value);
     Grade* grades=new Grade[100];  // Fixed-size array to store up to 100 grades
