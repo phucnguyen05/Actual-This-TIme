85dae6f451d9cd6bbc97bbe46ccf285e3b786219
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-10-08 16:04:57 +1030

    test

--- USBConnection.h
+++ USBConnection.h
@@ -6,37 +6,28 @@
 
 class USBConnection {
 private:
-    int ID; // Unique ID for the USB connection
+    int ID; 
 
-    // Private constructor
     USBConnection(int id) : ID(id) {}
 
 public:
-    // Public destructor to return the ID to the stack
     ~USBConnection() {
-        ids.push(ID); // Return the ID to the stack when the object is destroyed
+        ids.push(ID); 
     }
 
-    // Static function to create a new USBConnection object
     static USBConnection* create() {
         if (ids.empty()) {
-            return nullptr; // No available ID
+            return nullptr; 
         }
-        int id = ids.top(); // Get the available ID
-        ids.pop(); // Remove the ID from the stack
-        return new USBConnection(id); // Create and return the new object
+        int id = ids.top(); 
+        ids.pop(); 
+        return new USBConnection(id); 
     }
-
-    // Static stack to keep track of available IDs
     static std::stack<int> ids;
-
-    // Function to get the ID of the USB connection
     int getID() const {
         return ID;
     }
 };
-
-// Initialize the static stack with available IDs
 std::stack<int> USBConnection::ids({3, 2, 1});
 
 #endif // USBCONNECTION_H
