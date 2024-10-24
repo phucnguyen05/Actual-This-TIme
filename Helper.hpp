4ee049bf4bc043bd2e60b4d7d7a0e5d08d9c40b0
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-10-24 13:35:40 +1030

    1

--- Helper.hpp
+++ Helper.hpp
@@ -11,9 +11,9 @@ class Helper{
         int y1=std::get<1>(item1);
         int x2=std::get<0>(item2);
         int y2=std::get<1>(item2);
-        int x_result=(x1-x2)^2;
-        int y_result=(y1-y2)^2;
-        int combined_result=pow(x_result+y_result,0.5);
+        double x_result=(x1-x2)^2;
+        double y_result=(y1-y2)^2;
+        double combined_result=pow(x_result+y_result,0.5);
         return combined_result;
     }
 
