9b7ec05df50af74788c422c51e1661335d297b48
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-10-24 13:44:05 +1030

    1

--- Helper.hpp
+++ Helper.hpp
@@ -6,6 +6,7 @@
 
 #include <utility>
 class Helper{
+public:
     static double euclideanDistance(std::pair<int, int> item1, std::pair<int, int> item2){
         int x1=std::get<0>(item1);
         int y1=std::get<1>(item1);
