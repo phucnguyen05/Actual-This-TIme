59a6129cc35c6d4937a9028d2287150b00ded717
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-10-24 14:07:11 +1030

    2

--- Robot.hpp
+++ Robot.hpp
@@ -10,7 +10,10 @@ public:
     Robot(int gridWidth, int gridHeight){
         setCoordinates(0,0);
         this->health=3;
+        this->gridHeight=gridHeight;
+        this->gridWidth=gridWidth;
     }
+
     int getHealth(){
         return health;
     }
@@ -24,10 +27,27 @@ public:
         if(xOffset!=0 && yOffset!=0){
             return false;
         }
-        
+        if(xOffset!=0){
+            int current_x=coordinates.first;
+            if(current_x+xOffset>gridWidth-1){
+                return false;
+            }else{
+                coordinates.first=coordinates.first+xOffset;
+            }
+        }else{//y offset movement
+            int current_y=coordinates.second;
+            if(current_y+yOffset>gridHeight-1){
+                return false;
+            }else{
+                coordinates.second=coordinates.second+yOffset;
+            }
+        }
+
     }
 
     int health;
+    int gridWidth;
+    int gridHeight;
 };
 
 #endif
