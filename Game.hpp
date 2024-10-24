170ef8ed289cac625e7107ff8b52d90aed52c529
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-10-24 14:54:41 +1030

    fuck me

--- Game.hpp
+++ Game.hpp
@@ -1,6 +1,15 @@
 #ifndef GAMECLASS_H
 #define GAMECLASS_H 
+#include <utility>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
 class Game{
+public:
+    Game(int width, int height, std::vector<std::pair<int, int>> obstacleCoordinates){
+
+    }
 
 };
 
