537b1c3f060721bbefc421fb4fc4596e8f96e668
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2024-10-24 13:20:58 +1030

    1.1

---
+++ GridItem.hpp
@@ -0,0 +1,50 @@
+#ifndef GRIDITEM_H
+#define GRIDITEM_H
+#include <utility>
+
+class GridItem{
+public:
+    GridItem(int x, int y, int width, int height) : GridItem(){
+        coordinates={x,y};
+        this->width=width;
+        this->height=height;
+        GridItemCount++;
+    }
+
+    GridItem(){
+        width=0;
+        height=0;
+        coordinates={0,0};
+    }
+
+    void setCoordinates(int x, int y){
+        coordinates={x,y};
+    }
+
+    std::pair<int,int> getCoordinates(){
+        return coordinates;
+    }
+
+    int getGridWidth(){
+        return width;
+    }
+
+    int getGridHeight(){
+        return height;
+    }
+
+    int getActiveGridItemCount(){
+        return GridItemCount;
+    }
+
+    virtual ~GridItem(){
+        GridItemCount--;
+    }
+
+    int width;
+    int height;
+    std::pair<int,int> coordinates;
+    static int GridItemCount;
+};
+
+#endif
